b6a3a169395949d97aa6c330abdd44b9a3e7dde1
Marwa Ters <marwaters94@gmail.com>
2024-06-24 11:58:36 +0200

    creation Email,attributs et methode à completer

---
+++ Projet_aideComptable/Email.java
@@ -0,0 +1,17 @@
+package Projet_aideComptable;
+
+import java.util.Date;
+
+public class Email {
+    private int id;
+    private Client destinataire;
+    private Date datetime;
+    private String sujet;
+    private String corps;
+    private Facture facture;
+
+    public void envoyerFacture() {
+      
+    }
+    
+}
