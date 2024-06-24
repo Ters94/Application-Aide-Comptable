4777fcee2a9dcf094faf33f049ea9739c92cf5f4
Marwa Ters <marwaters94@gmail.com>
2024-06-24 11:24:21 +0200

    ajout des attribut et les methodes a completer de user

--- Projet_aideComptable/Utilisateur.java
+++ Projet_aideComptable/Utilisateur.java
@@ -2,5 +2,22 @@ package Projet_aideComptable;
 
 public class Utilisateur {
     
+    private int id;
+    private String nom;
+    private String adresse;
+    private String email;
+    private String role;
+
+public void createUser(){
+
+}
+public void managePermission(){
+
+}
+public void createAccount(){
+
+}
+public void manageAccount(){
     
+}
 }
