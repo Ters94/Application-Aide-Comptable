7ec293bd858e627be7f71108c97153ea5769c739
Marwa Ters <marwaters94@gmail.com>
2024-06-24 11:39:20 +0200

    creation class Fournisseur et les methodes à completer

---
+++ Projet_aideComptable/Fournisseur.java
@@ -0,0 +1,20 @@
+package Projet_aideComptable;
+
+public class Fournisseur extends Utilisateur {
+
+     public void etatTransaction() {
+        
+    }
+
+    public void creerFacture() {
+        
+    }
+
+    public void editerFacture() {
+     
+    }
+
+    public void supprimerFacture() {
+     
+    }
+}
