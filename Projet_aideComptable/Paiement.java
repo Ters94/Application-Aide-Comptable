b9c6bd8ca719e16c8bf23ff1e6ac7eda41916298
Marwa Ters <marwaters94@gmail.com>
2024-06-24 11:56:48 +0200

    creation de la classe paiement avec les attributs et les methodes à completer

---
+++ Projet_aideComptable/Paiement.java
@@ -0,0 +1,26 @@
+package Projet_aideComptable;
+
+import java.util.Date;
+
+public class Paiement {
+    private int id;
+    private Facture facture;
+    private Date dateTime;
+    private double montant;
+
+    public void enregistrerPaiement() {
+      
+    }
+
+    public void genererRecu() {
+    }
+
+    public void genererAccuseReception() {
+      
+    }
+
+    public void genererRappels() {
+     
+    }
+    
+}
