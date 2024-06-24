46f7c3a4cf16578de7e508ca76900912c0b83ed3
Marwa Ters <marwaters94@gmail.com>
2024-06-24 11:54:10 +0200

    Creation class facture avec les attributs

---
+++ Projet_aideComptable/Facture.java
@@ -0,0 +1,12 @@
+package Projet_aideComptable;
+
+import java.util.Date;
+
+public class Facture {
+    private int id;
+    private Client client;
+    private Date dateTime;
+    private double montant;
+    private String etatFacture;
+
+}
