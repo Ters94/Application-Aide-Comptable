d68788b7ec27118a85bef0eab7b84998a6529a76
Marwa Ters <marwaters94@gmail.com>
2024-06-25 12:43:07 +0200

    correction client=>utilisateur

--- Projet_aideComptable/accountinghelper/src/main/java/com/poo/accountinghelper/UserController.java
+++ Projet_aideComptable/accountinghelper/src/main/java/com/poo/accountinghelper/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 public class UserController {
-    @GetMapping("/client/{id}")
+    @GetMapping("/utilisateur/{id}")
     // pour retourner un objet : 
     //on a rajouter le path parcequ'on a pris le id du getmapping precedent 
-    public ResponseEntity<Client> getClient(@PathVariable int id){
+    public ResponseEntity<Utilisateur> getUtilisateur(@PathVariable int id){
+        //essai de creer un objet
+        Utilisateur result = new Utilisateur();
         return null;
 
     }
