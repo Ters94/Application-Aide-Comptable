e9bda78b735f7624abe20277f849d060c103e913
Marwa Ters <marwaters94@gmail.com>
2024-06-25 12:32:32 +0200

    api avec quelque reglage du settings

--- Projet_aideComptable/accountinghelper/src/main/java/com/poo/accountinghelper/UserController.java
+++ Projet_aideComptable/accountinghelper/src/main/java/com/poo/accountinghelper/UserController.java
@@ -1,5 +1,17 @@
 package com.poo.accountinghelper;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 public class UserController {
-    
+    @GetMapping("/client/{id}")
+    // pour retourner un objet : 
+    //on a rajouter le path parcequ'on a pris le id du getmapping precedent 
+    public ResponseEntity<Client> getClient(@PathVariable int id){
+        return null;
+
+    }
+
 }
