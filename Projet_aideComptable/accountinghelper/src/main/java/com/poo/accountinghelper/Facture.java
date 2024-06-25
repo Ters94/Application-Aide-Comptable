f9f1dcf1085aa9aa22ae3c18d6bb85969174c006
Marwa Ters <marwaters94@gmail.com>
2024-06-25 11:12:54 +0200

    getters and setters

--- Projet_aideComptable/accountinghelper/src/main/java/com/poo/accountinghelper/Facture.java
+++ Projet_aideComptable/accountinghelper/src/main/java/com/poo/accountinghelper/Facture.java
@@ -15,5 +15,47 @@ public class Facture {
       this.montant = montant;
       this.client = client;
     }
+/*------------------------------------ */
+/* Getters and setters */
+/*---------------------------------------- */
 
+public int getId() {
+  return id;
+}
+
+public void setId(int id) {
+  this.id = id;
+}
+
+public Client getClient() {
+  return client;
+}
+
+public void setClient(Client client) {
+  this.client = client;
+}
+
+public LocalDate getDate() {
+  return date;
+}
+
+public void setDate(LocalDate date) {
+  this.date = date;
+}
+
+public double getMontant() {
+  return montant;
+}
+
+public void setMontant(double montant) {
+  this.montant = montant;
+}
+
+public String getEtatFacture() {
+  return etatFacture;
+}
+
+public void setEtatFacture(String etatFacture) {
+  this.etatFacture = etatFacture;
+}
 }
